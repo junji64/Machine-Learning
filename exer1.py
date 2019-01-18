749554d384aa315707f8efd112aa6635aa1c9a2c
Jun <Jun@Jun-ui-MacBook-Air.local>
2019-01-18 23:52:19 +0900

    Lab4 inserted

---
+++ exer1.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-5, 5, 0.1)
+y = np.arange(-5, 5, 0.1)
+xx, yy = np.meshgrid(x, y, sparse=True)
+z = np.sin(xx**2 + yy**2) / (xx**2 + yy**2)
+h = plt.contourf(x,y,z)
